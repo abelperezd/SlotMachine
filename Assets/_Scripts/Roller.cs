784d2696c4f62d5c7d073a9f1ac57cf58cb852ba
abelperezd <abelperdan@gmail.com>
2023-04-19 22:52:25 +0200

    Working, cleaned an improved!

--- Assets/_Scripts/Roller.cs
+++ Assets/_Scripts/Roller.cs
@@ -24,54 +24,29 @@ public class Roller : MonoBehaviour
 
     }
 
-    public void StartSpinning(int spins, float duration)
+    public void StartSpinning(SpinningConfiguration config)
     {
-        StartCoroutine(Spin(spins, duration));
+        StartCoroutine(Spin(config));
     }
 
     public int stepsPerFigure;
-    IEnumerator Spin(int spinsssss, float durationn)
+    IEnumerator Spin(SpinningConfiguration config)
     {
-        float figureSize = 2.2f; // units/fig
-
-        float duration = 5; //  sec
-
-        int vel = 8; // fig/sec
-
-        int figuresToSee = (int)(vel * duration); //    figs
-
-        //int stepsPerFigure = 22; //  steps/figure
-
-        int stepsToDo = figuresToSee * stepsPerFigure; //   steps
-
-        float velInUnitsPerStep = vel * (float)figureSize * (float)duration / (float)stepsToDo; // units/step
-
-        countTime = true;
-
-        float slowDownPercentage = 0.2f;
-
-        int slowDownFigures = (int)Mathf.Ceil(vel * slowDownPercentage);
-
-        int lastSteps = slowDownFigures * stepsPerFigure;
 
         int extraSubSteps = 1;
 
-        int extraSubstepsFactor = vel / slowDownFigures;
-
-
-        for (int j = 0; j < stepsToDo; j++)
+        for (int j = 0; j < config.stepsToDo; j++)
         {
             for (int i = 0; i < extraSubSteps; i++)
             {
                 TryRestart();
-                transform.position -= Vector3.up * velInUnitsPerStep / extraSubSteps;
+                transform.position -= Vector3.up * config.velInUnitsPerStep / extraSubSteps;
                 //yield return null;
-                yield return new WaitForSeconds(duration / stepsToDo / extraSubSteps);
+                yield return new WaitForSeconds(config.duration / config.stepsToDo / extraSubSteps);
             }
-            if (j > (stepsToDo - lastSteps))
-                extraSubSteps += extraSubstepsFactor;
+            if (j > (config.stepsToDo - config.slowDownSteps) && j%config.visualizationFrequency == 0)
+                extraSubSteps += config.extraSubstepsFactor;
         }
-        countTime = false;
     }
 
     private void TryRestart()
--- Assets/_Scripts/RollerManager.cs
+++ Assets/_Scripts/RollerManager.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct SpinningConfiguration
+{
+    public int stepsToDo;
+    public float velInUnitsPerStep;
+    public float duration;
+    public int slowDownSteps;
+    public int extraSubstepsFactor;
+    public int visualizationFrequency;
+}
+
 public class RollerManager : MonoBehaviour
 {
+    // units/figure
     private const float FIGURE_SIZE = 2.2f;
 
     [SerializeField] private List<Roller> _rollers;
@@ -11,17 +22,25 @@ public class RollerManager : MonoBehaviour
     [Tooltip("Figures/second")]
     [SerializeField] private int _spinVelocity = 8;
 
-    [Tooltip("Steps/figure")]
-    [SerializeField] private int _resolution = 8;
-
     [Tooltip("Seconds")]
     [SerializeField] private float _delayBetweenRollers = .5f;
 
-    [SerializeField] private float _slowDownDuration = 2f;
 
     [SerializeField] private float _minSpinningDuration = 2;
 
     [SerializeField] private float _maxSpinningDuration = 4;
+
+    [Range(0, 1)]
+    [Tooltip("Max duration difference there can be between 2 consecutive rollers. " +
+        "It will be clamped with the Delay Between Rollers")]
+    [SerializeField] private float _maxSpinningDurationDelayBetweenConsecutiveRollers = .1f;
+
+    [Range(0, 1)]
+    [Tooltip("Percentage of the total time that the roller will be slowing down")]
+    [SerializeField] private float _slowDownPercentage = .2f;
+
+    [Tooltip("Steps we do for every figure")]
+    [SerializeField] private int _visualizationFrequency = 8;
 
     private void StartSpinningRollers()
     {
@@ -34,8 +53,8 @@ public class RollerManager : MonoBehaviour
         {
             int spins;
             float duration;
-            (spins, duration) = GetSpinsAndDuration();
-            r.StartSpinning(spins, duration);
+            SpinningConfiguration configuration = GetSpinningConfiguration(GetSpinDuration());
+            r.StartSpinning(configuration);
             yield return new WaitForSeconds(_delayBetweenRollers);
         }
     }
@@ -46,28 +65,39 @@ public class RollerManager : MonoBehaviour
         StartSpinningRollers();
     }
 
-    private (int, float) GetSpinsAndDuration()
+    private float GetSpinDuration()
     {
-        float randomDuration = Random.Range(_minSpinningDuration, _maxSpinningDuration);
-        //        Debug.Log("randomDuration: " + randomDuration);
-
+        float randomDuration = Random.Range(_minSpinningDuration, _maxSpinningDuration) +
+            Mathf.Clamp(Random.Range(0, _maxSpinningDurationDelayBetweenConsecutiveRollers), 0, _delayBetweenRollers);
+        //Debug.Log("randomDuration: " + randomDuration);
         int spins = Mathf.RoundToInt(_spinVelocity * randomDuration);
-        //Debug.Log("spin velocity: " + SpinVelocity);
-
-        //Debug.Log("spins: " + spins);
-
-        float spinDuration = (float)spins / _spinVelocity;
-        //      Debug.Log("spin duration: " + spinDuration);
-        return (spins, spinDuration);
+        return (float)spins / _spinVelocity;
     }
 
-    private float GetSlowDownDuration()
+    private SpinningConfiguration GetSpinningConfiguration(float spinDuration)
     {
-        int spins = Mathf.RoundToInt(_spinVelocity * _slowDownDuration);
-        Debug.Log("spins " + spins);
-        float spinDuration = (float)spins / _spinVelocity;
-        // Debug.Log("spin duration " + spinDuration);
-        return spinDuration;
+
+        int figuresToSee = (int)(_spinVelocity * spinDuration); //    figs
+
+        int stepsToDo = figuresToSee * _visualizationFrequency; //   steps
+
+        float velInUnitsPerStep = _spinVelocity * FIGURE_SIZE * spinDuration / (float)stepsToDo; // units/step
+
+        int slowDownFigures = (int)Mathf.Ceil(figuresToSee * _slowDownPercentage);
+
+        int slowDownSteps = slowDownFigures * _visualizationFrequency;
+
+        int extraSubstepsFactor = _spinVelocity / slowDownFigures;
+
+        return new SpinningConfiguration
+        {
+            stepsToDo = stepsToDo,
+            velInUnitsPerStep = velInUnitsPerStep,
+            duration = spinDuration,
+            slowDownSteps = slowDownSteps,
+            extraSubstepsFactor = extraSubstepsFactor,
+            visualizationFrequency = _visualizationFrequency
+        };
     }
 
 }
